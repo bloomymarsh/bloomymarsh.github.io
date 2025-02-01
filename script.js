2f87a65bd743afd67b3dd828064d4c734d2fa0d7
Marsha Fabiola <144167717+bloomymarsh@users.noreply.github.com>
2025-02-01 18:59:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -385,18 +385,19 @@ let TxtType = function(el, toRotate, period) {
         that.tick();
         }, delta);
     };
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-        // INJECT CSS for trailer
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.11em solid #eee}";
-        document.body.appendChild(css);
-    };
+
+    document.addEventListener("DOMContentLoaded", function() {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS for trailer
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.11em solid #eee}";
+  document.body.appendChild(css);
+}); // <-- Fixed closing bracket and parenthesis
